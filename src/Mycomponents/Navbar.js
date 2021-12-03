1e2553830b9635131a6600a88bae9119de4ebf6c
umar <umaramjad489@gmail.com>
2021-12-03 08:02:00 -0800

    sevenchanges

--- src/Mycomponents/Navbar.js
+++ src/Mycomponents/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light shadow bg-dark opacity50 fixed-top" >
   <div className="container-fluid" >
-    <Link className="navbar-brand text-white fw-bolder webtitle" smooth to="#home">
+    <Link className="navbar-brand text-white fw-bolder webtitle" smooth to="/">
       "Education Learning"<i class="fas fa-university"></i></Link>
     <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -15,7 +15,7 @@ export default function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" smooth to="#home">Home</Link>
+          <Link className="nav-link" smooth to="/">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" smooth  to="#about">About</Link>
